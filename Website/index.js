9f94b7cb5fd9a3482270bb42ecdad859f0a84535
Carpmenter <nickcarpenter1017@gmail.com>
2022-10-03 15:50:48 -0500

    Add cloud resume website files

---
+++ Website/index.js
@@ -0,0 +1,25 @@
+async function counter_fn() {
+    var counter = document.getElementById("cntr");
+    var url = 'https://l8q743vdmb.execute-api.us-east-1.amazonaws.com/prod'
+
+    //GET current count of entries from DB
+    const getResponse = await fetch(url);
+    const getJson = await getResponse.json();
+    console.log(getJson.body.length);
+    var count = getJson.body.length;
+    count++;
+    counter.innerHTML = count;
+
+    //POST updated count to DB
+    // const postResponse = await fetch(url, {
+    //     method: 'POST',
+    //     mode: 'cors',
+    //     body: JSON.stringify({
+    //         "countId": count
+    //     })
+    // });
+    // const postJson = await postResponse.json();
+    // console.log(postJson);
+
+  }
+  window.onload = counter_fn;
